b0c383bb6423fbdb50a17114a49a3856a628dbf5
Giancarlo <giancarlo0015@gmail.com>
2021-03-31 15:23:15 -0600

    Changed include order, so sessioncheck is first

--- homepage/index.php
+++ homepage/index.php
@@ -2,10 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-include_once('../View/Common/header.php');
 include_once('../Control/ControlSessionCheck.php');
-
+include_once('../View/Common/header.php');
 include_once('../Model/ModelGame.php');
 
 $game = new Game();
