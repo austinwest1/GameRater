651b6904b22fa00ecc5a74892782fc782d19b630
Austin West <austinwest1@mail.weber.edu>
2021-04-12 16:18:28 -0600

    Changed header title to 'Game Rater'

--- register.php
+++ register.php
@@ -29,7 +29,7 @@ error_reporting(E_ALL);
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#">My Games</a>
+            <a class="navbar-brand" href="#">Game Rater</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
